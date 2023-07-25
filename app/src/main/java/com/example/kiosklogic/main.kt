5fc1baf41709c356a9b5d41da12c940bdcca7f5a
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-07-25 21:23:19 +0900

    practice map

--- app/src/main/java/com/example/kiosklogic/main.kt
+++ app/src/main/java/com/example/kiosklogic/main.kt
@@ -1,3 +1,9 @@
+import android.provider.Settings.Global.getString
+import com.example.kiosklogic.MapInfromation
+import com.example.kiosklogic.R
+
 fun main(){
-    print("main")
+    println("main")
+    var a =MapInfromation()
+    a.lunch()
 }
