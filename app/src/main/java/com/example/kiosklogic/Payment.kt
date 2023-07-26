564d5a173771164d038cdff75cd4e19da4a8a142
yeonjooRyu <yeonjoo3751@gmail.com>
2023-07-26 14:37:01 +0900

    charge commit

---
+++ app/src/main/java/com/example/kiosklogic/Payment.kt
@@ -0,0 +1,19 @@
+package com.example.kiosklogic
+
+class Payment {
+    val rental1h = 1000
+    val rental2h = 2000
+    val rental3h = 3000
+    val rental4h = 4000
+    val rental5h = 5000
+    val rental24h = 6000
+
+    fun payment(){
+    println("나의 금액을 입력하세요")
+    val myMoney = readLine()!!.toInt()
+
+
+    }
+
+
+}
