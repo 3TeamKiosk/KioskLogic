3587e2aea3d833c94877a8615c1aeadf9275303b
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-07-26 19:57:31 +0900

    renewal

--- app/src/main/java/com/example/kiosklogic/main.kt
+++ app/src/main/java/com/example/kiosklogic/main.kt
@@ -1,9 +1,7 @@
-import android.provider.Settings.Global.getString
-import com.example.kiosklogic.MapInfromation
-import com.example.kiosklogic.R
+import com.example.kiosklogic.mapInformation
+import com.example.kiosklogic.mapInformationAdmin
 
 fun main(){
     println("main")
-    var a =MapInfromation()
-    a.lunch()
+    mapInformationAdmin()
 }
