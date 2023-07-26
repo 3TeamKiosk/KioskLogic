92b5cb1262496d9f586996d7f0572a1b5724597b
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-07-26 14:26:11 +0900

    주석 처리&취소 시 처리

--- app/src/main/java/com/example/kiosklogic/MapInfromation.kt
+++ app/src/main/java/com/example/kiosklogic/MapInfromation.kt
@@ -1,8 +1,5 @@
 package com.example.kiosklogic
 
-import java.util.concurrent.ThreadLocalRandom
-import kotlin.random.Random
-
 class MapInfromation {
     var stationName: MutableList<String> = mutableListOf("엑스", "펙토", "패트", "매트", "로눔")
     var stationDescription: MutableList<String> = mutableListOf(
@@ -19,10 +16,10 @@ class MapInfromation {
         "bicycleCount" to bicycleCount.toMutableList(),
     )
 
-    fun lunch() {
+    fun lunch() {   // 지도 기능을 실행 시 진행되는 코드
         var num: Int?
         println("지도를 선택하고 역을 선택하시면 자세하게 안내해드립니다.")
-        println(
+        println(    // 역 위치를 표시하는 지도
             "       .--                    --                                        ,---~~-,--                --\n" +
                     "       ,--                    ~-                                    .,~~-~~-    ,~.                ~\n" +
                     "       ,-,                    ~-                                 .-:~-~~,       ,:.                -\n" +
@@ -82,44 +79,46 @@ class MapInfromation {
                     ".:!;           :~      -:;,  ~~.,                         ,:                     -~                 \n"
         )
         for (i in 0..4) {
-            print("[${i + 1}] ${getStatinName(i)}역 ")
+            print("[${i + 1}] ${getStationName(i)}역 ")
         }
         println("[0]취소")
-        while (true) {
+        while (true) {  // 원하는 num이 들어올 때까지 실행
             print("입력 : ")
-            try {
+            try {   // 숫자가 아닌 입력 시 에러
                 num = readLine()?.toInt()
             } catch (e: NumberFormatException) {
                 println("숫자가 아닙니다. 0 ~ 5를 입력해주세요")
                 continue
             }
-            if (num in 0..6) break
-            else println("유효한 값이 아닙니다. 0 ~ 5를 입력해주세요")
+            if (num in 0..6) break  // 0~6의 숫자가 나올 시 반복문 나오기
+            else println("유효한 값이 아닙니다. 0 ~ 5를 입력해주세요")  // 0~6의 숫자가 아닐 시 알리기
         }
-        if (num == 6) for (i in 0..4) println(getStationInformation(i) + "\n")
-        else println(num?.let { getStationInformation(it) })
+        if (num == 6) for (i in 0..4) println(getStationInformation(i) + "\n")  // 6은 모든 역의 정보를 가져오기
+        else println(num?.let { getStationInformation(it) })    // 1~5 선택한 역 정보 가져오기
 //        println("4K${Math.random().toInt()}${(100..999).random()}${ThreadLocalRandom.current().nextInt(100,999)}${Random.nextInt()}")
+        if (num==0) print("main으로") // 0 취소 버튼을 클릭 시 main으로 돌아가기
+
     }
 
-    fun getStationInformation(index: Int): String {
+    fun getStationInformation(index: Int): String { // 역 설명 이름&위치설명&사용가능한 자전거 수
         return "${index + 1}번째 역은 ${stationInformation["name"]?.get(index)}역입니다.\n" +
                 "위치는 ${stationInformation["description"]?.get(index)}에 위치해 있습니다.\n" +
-                "현 남아있는 자전거 수는 ${stationInformation["bicycleCount"]?.get(index)}개입니다."
+                "현재 사용가능한 자전거 수는 ${stationInformation["bicycleCount"]?.get(index)}개입니다."
     }
 
-    fun setStationName(index: Int, name: String) {
+    fun setStationName(index: Int, name: String) {  // 역 이름 변경 (관리자모드)
         stationName[index] = name
     }
 
-    fun setStationDescription(index: Int, des: String) {
+    fun setStationDescription(index: Int, des: String) {    // 역 위치 설명 변경 (관리자모드)
         stationDescription[index] = des
     }
 
-    fun setBicycleCount(index: Int, count: Int) {
+    fun setBicycleCount(index: Int, count: Int) {   // 사용가능한 자전거 수 변경
         bicycleCount[index] = count
     }
 
-    fun getStatinName(index: Int): String {
+    fun getStationName(index: Int): String { // 역 이름 가져오기
         return stationName[index]
     }
 }
