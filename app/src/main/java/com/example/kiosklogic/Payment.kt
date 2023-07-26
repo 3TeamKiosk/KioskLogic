9acdbdc95964d85e1f0c7d6d5bc091b4551c7b90
yeonjooRyu <yeonjoo3751@gmail.com>
2023-07-26 20:49:59 +0900

    charge commit

--- app/src/main/java/com/example/kiosklogic/Payment.kt
+++ app/src/main/java/com/example/kiosklogic/Payment.kt
@@ -1,19 +1,40 @@
 package com.example.kiosklogic
 
 class Payment {
-    val rental1h = 1000
-    val rental2h = 2000
-    val rental3h = 3000
-    val rental4h = 4000
-    val rental5h = 5000
-    val rental24h = 6000
+    var myMoney = 10000
+    var moneyList = listOf(0, 1000, 2000, 3000, 4000, 5000)
+    var rentalHour = readLine()!!.toInt()
 
-    fun payment(){
-    println("나의 금액을 입력하세요")
-    val myMoney = readLine()!!.toInt()
+
+    fun payment() {
+        while (true) {
+    try {
+        if (rentalHour >= 0 && rentalHour < moneyList.size) {
+            val valueAtIndex = moneyList[rentalHour]
+            println("${rentalHour}시간 대여료 : ${valueAtIndex}원 결제하시겠습니까?")
+            println("[1]예 [2]아니오")
+            var check = readLine()!!.toInt()
+            var change = myMoney - valueAtIndex
+            if (check == 1) {
+                if (myMoney >= valueAtIndex) {
+                    println("[대여 후 잔액]: ${change}원입니다.")
+                } else {
+                    println("금액이 부족합니다.")
+                    return main()
+                }
+            } else if (check == 2){
+                println("메인화면으로 이동합니다.")
+                return main()
+            } else{
+                println("1, 2의 숫자중에서 다시 입력하세요.")
+                return payment()
+            }
+        }; break
+    } catch (e: java.lang.NumberFormatException) {
+        println("1, 2의 숫자중에서 다시 입력하세요.")
+    }
+}
 
 
     }
-
-
 }
--- app/src/main/java/com/example/kiosklogic/main.kt
+++ app/src/main/java/com/example/kiosklogic/main.kt
@@ -1,18 +1,80 @@
 package com.example.kiosklogic
-fun main(){
-    println("[요금 안내]")
-    println("[1]1시간 대여 : 1,000원")
-    println("[2]2시간 대여 : 2,000원")
-    println("[3]3시간 대여 : 3,000원")
-    println("[4]4시간 대여 : 4,000원")
-    println("[5]5시간 대여 : 5,000원")
-    println("[6]24시간 대여: 10,000원")
-    println("==============================")
-    println("[사용 시간 선택 및 결제하기]")
-    println("사용 시간을 선택해주세요.")
+fun main() {
 
-    var money = Charge()
-    money.charge()
+    println("[1]요금안내 [2]결제하기 [3]메인")
+    var checkNum1 = readLine()!!.toInt()
+    while (true) {
+        try {
+            if(checkNum1 == 1){
+                var charge = ChargeInfo()
+                charge.chargeInfo()
+                var checkNum2 = readLine()!!.toInt()
+                if(checkNum2 == 1){
+                    println("대여 시간을 입력해주세요")
+                    var pay = PaymentInfo()
+                    pay.paymentInfo()
+                    var money = Payment()
+                    money.payment()
+                } else if(checkNum2 == 2){
+                    println("메인으로 이동합니다")
+                    return main()
+                } else{
+                    println("다시 입력하세요")
+                    return main()
+                }
 
+            } else if(checkNum1 == 2){
+                println("대여 시간을 입력해주세요")
+                var pay = PaymentInfo()
+                pay.paymentInfo()
+                var money = Payment()
+                money.payment()
+            } else if(checkNum1 == 3){
+                println("요금안내 및 결제화면 메인으로 이동합니다")
+                return main()
+            } else {
+                println("다시 입력하세요.")
+                return main()
+            }; break
+
+        } catch (e: java.lang.NumberFormatException) {
+            println("잘못된 입력입니다.")
+        }
 
     }
+}
+
+
+
+
+
+//    var checkNum = readLine()!!.toInt()
+//    while(true){
+//        try {
+//            if(checkNum == 1){
+//                println("대여 시간을 입력해주세요")
+//                println("[1]1시간 대여")
+//                println("[2]2시간 대여")
+//                println("[3]3시간 대여")
+//                println("[4]4시간 대여")
+//                println("[5]5시간 대여")
+//                var money = Payment()
+//                money.payment()
+//            } else if(checkNum == 2) {
+//                println("메인으로 이동합니다.")
+//                return main()
+//            } else {
+//                println("다시 입력하세요.")
+//                return main()
+//            }; break
+//        } catch (e: java.lang.NumberFormatException) {
+//            println("다시 입력하세요.")
+//        }
+//    }
+
+
+
+
+
+
+
