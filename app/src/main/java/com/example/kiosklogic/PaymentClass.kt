d25b39b8761dac61f81014da843389a790faa029
iWantBigMan <skarndgus8899@gmail.com>
2023-07-27 21:35:58 +0900

    feature/payment correct

--- app/src/main/java/com/example/kiosklogic/PaymentClass.kt
+++ app/src/main/java/com/example/kiosklogic/PaymentClass.kt
@@ -1,14 +1,14 @@
 package com.example.kiosklogic
 
 class PaymentClass: PaymentInformation<String, Int> {
-    var rentalTime = mapOf(
+   private var rentalTime = mapOf(
         "[1]" to "1시간 대여",
         "[2]" to "2시간 대여",
         "[3]" to "3시간 대여",
         "[4]" to "4시간 대여",
         "[5]" to "5시간 대여"
     )
-    var rentalMoney = mutableMapOf("1" to 1000, "2" to 2000, "3" to 3000, "4" to 4000, "5" to 5000)
+  var rentalMoney = mutableMapOf("1" to 1000, "2" to 2000, "3" to 3000, "4" to 4000, "5" to 5000)
 
     override fun chargeInfo() {
         rentalMoney.forEach { (k, v) ->
@@ -23,4 +23,46 @@ class PaymentClass: PaymentInformation<String, Int> {
             println("$k $v")
         }
     }
-}
+   fun payment() {
+         var rentalTime = readLine()!!.toString()
+        while (true) {
+            try {
+                val valueAtIndex = rentalMoney["$rentalTime"]
+                if (valueAtIndex != null) {
+                    println("${rentalTime}시간 대여료 : ${valueAtIndex}원 결제하시겠습니까?")
+                    println("[1]예 [2]아니오")
+                    var check = readLine()!!.toInt()
+                    var myMoney = 3000
+                    var change = myMoney - valueAtIndex
+                    when (check) {
+                        1 -> {
+                            if (myMoney >= valueAtIndex) {
+                                println("[나의 금액]: ${myMoney}원")
+                                println("[대여 후 나의 잔액]: ${change}원입니다.")
+                            } else {
+                                println("[나의 금액]: ${myMoney}원")
+                                println("${valueAtIndex - myMoney} 원 부족합니다.")
+                                println("결제 실패. 메인으로 이동합니다.")
+                                return main()
+                            }
+                            return main()
+                        }
+                        2 -> {
+                            println("메인화면으로 이동합니다.")
+                            return main()
+                        }
+                        else -> {
+                            println("1, 2의 숫자중에서 다시 입력하세요.")
+                            return payment(rentalMoney)
+                        }
+                    }
+                } else {
+                    println("1~5 사이의 숫자를 입력하세요.")
+                    return payment(rentalMoney)
+                }
+            } catch (e: NumberFormatException) {
+                println("1~5 사이의 숫자를 입력하세요.")
+            }
+        }
+    }
+    }
--- app/src/main/java/com/example/kiosklogic/main.kt
+++ app/src/main/java/com/example/kiosklogic/main.kt
@@ -9,55 +9,63 @@ import java.util.Locale
 package com.example.kiosklogic
 
 fun main() {
+    var payService = PaymentClass()
 
     println("[1]요금안내 [2]결제하기 [3]메인 [4]관리자모드")
     var checkNum1 = readLine()!!.toInt()
     while (true) {
         try {
             // [1]요금 안내를 선택했을 때의 화면
-            if(checkNum1 == 1){
-                var charge = ChargeInfo()
-                charge.chargeInfo()
-                while(true){
-                    // 요금안내 화면 안에서 결제하기 or 메인으로 돌리는 코드
-                    var checkNum2 = readLine()!!.toInt()
-                    if(checkNum2 == 1){
-                        println("대여 시간을 입력해주세요")
-                        var pay = PaymentInfo()
-                        pay.paymentInfo()
-                        var money = Payment()
-                        money.payment()
-                    } else if(checkNum2 == 2){
-                        println("메인으로 이동합니다")
-                        return main()
-                    } else{
-                        println("다시 입력하세요")
-                        charge.chargeInfo()
+            when (checkNum1) {
+                1 -> {
+                  payService.chargeInfo()
+                    while(true){
+                        // 요금안내 화면 안에서 결제하기 or 메인으로 돌리는 코드
+                        var checkNum2 = readLine()!!.toInt()
+                        when (checkNum2) {
+                            1 -> {
+                                println("대여 시간을 입력해주세요")
+                                payService.paymentInfo()
+                                payService.payment()
+
+
+                            }
+                            2 -> {
+                                println("메인으로 이동합니다")
+                                return main()
+                            }
+                            else -> {
+                                println("다시 입력하세요")
+                                payService.chargeInfo()
+                            }
+                        }
                     }
+                    //메인에서 [2]결제하기로 바로갔을 때의 화면
                 }
-                //메인에서 [2]결제하기로 바로갔을 때의 화면
-            } else if(checkNum1 == 2){
-                println("대여 시간을 입력해주세요")
-                var pay = PaymentInfo()
-                pay.paymentInfo()
-                var money = Payment()
-                money.payment()
-                //1~5까지의 숫자나 정수가 아니면 다시 입력하게 함
-//                when(readLine()?.toIntOrNull()){
-//                    1,2,3,4,5 -> money.payment()
-//                    else -> println("다시 입력하세요.")
-//                }
-                // 메인으로 이동
-            } else if(checkNum1 == 3){
-                println("요금안내 및 결제화면 메인으로 이동합니다")
-                return main()
-                // 관리자 모드로 이동
-            } else if(checkNum1 == 4){
-                var admin = AdminCharge()
-                admin.changeMoney()
-            } else {
-                println("다시 입력하세요.")
-                return main()
+                2 -> {
+                    println("대여 시간을 입력해주세요")
+                    payService.paymentInfo()
+                    payService.payment()
+                    //1~5까지의 숫자나 정수가 아니면 다시 입력하게 함
+        //                when(readLine()?.toIntOrNull()){
+        //                    1,2,3,4,5 -> money.payment()
+        //                    else -> println("다시 입력하세요.")
+        //                }
+                    // 메인으로 이동
+                }
+                3 -> {
+                    println("요금안내 및 결제화면 메인으로 이동합니다")
+                    return main()
+                    // 관리자 모드로 이동
+                }
+                4 -> {
+                    var admin = AdminCharge()
+                    admin.changeMoney()
+                }
+                else -> {
+                    println("다시 입력하세요.")
+                    return main()
+                }
             }; break
 
         } catch (e: java.lang.NumberFormatException) {
