4d858925c99f3bfc525f7c098b411235365c89af
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-07-27 17:34:15 +0900

    finish

--- app/src/main/java/com/example/kiosklogic/ChangeMode.kt
+++ app/src/main/java/com/example/kiosklogic/ChangeMode.kt
@@ -1,20 +1,19 @@
 package com.example.kiosklogic
 
-import java.util.Locale
-
 fun changeMode() {
-    println("현재 모드입니다.")
+    val modeData = ChangeModeData.getInstance()
+    println("현재 ${modeData.getMode()}모드입니다.")
     print("변경하시겠습니까? [y/n(메인메뉴)]: ")
     while (true) {
         val input = readLine()
-        if (input=="yes"||input=="y"){
-            print("모드 변환")
-            break
+        when (input) {
+            "yes", "y" -> {
+                modeData.setMode()
+                break
+            }
+
+            "no", "n" -> break
+            else -> print("y/n 으로 입력해주세요: ")
         }
-        if (input=="no"||input=="n"){
-            print("홈으로")
-            break
-        }
-        print("y/n 으로 입력해주세요: ")
     }
 }
--- app/src/main/java/com/example/kiosklogic/ChangeModeData.kt
+++ app/src/main/java/com/example/kiosklogic/ChangeModeData.kt
@@ -2,13 +2,21 @@ package com.example.kiosklogic
 
 class ChangeModeData : ChangeModeInterface {
     var nowMode="사용자"
-    var modeType : List<String> = listOf("사용자","관리자")
+    companion object {  // 싱글턴
+        @Volatile private var instance: ChangeModeData? = null
+
+        fun getInstance(): ChangeModeData { // 다 같은 interface를 사용하게 유도
+            return instance ?: synchronized(this) {
+                instance ?: ChangeModeData().also { instance = it }
+            }
+        }
+    }
     override fun getMode(): String {
         return nowMode
     }
 
     override fun setMode() {
-        
+        nowMode = if(nowMode=="사용자") "관리자" else "사용자"
     }
 
 }
